88cebadab16e2044976af9c34f26a74ffa742e0f
JSKeum <jskeum2512@gmail.com>
2021-11-11 16:54:26 +0900

    fix helloworld exemple with instruction_data & changed account data

--- helloworld/src/lib.rs
+++ helloworld/src/lib.rs
@@ -13,6 +13,21 @@ use solana_program::{
 pub struct GreetingAccount {
     /// number of greetings
     pub counter: u32,
+    pub text: String
+}
+
+impl GreetingAccount {
+    fn new(counter: u32, text: String) -> Self { 
+        GreetingAccount {
+            counter, text
+        }
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Message {
+    pub text: String,
+    pub from: String
 }
 
 // Declare and export the program's entrypoint
@@ -22,7 +37,7 @@ entrypoint!(process_instruction);
 pub fn process_instruction(
     program_id: &Pubkey, // Public key of the account the hello world program was loaded into
     accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+    instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
 ) -> ProgramResult {
     msg!("Hello World Rust program entrypoint");
 
@@ -38,12 +53,28 @@ pub fn process_instruction(
         return Err(ProgramError::IncorrectProgramId);
     }
 
+    let message = Message::try_from_slice(instruction_data).map_err(|err| {
+        msg!("Receiveing message failed, {:?}", err);
+        ProgramError::InvalidInstructionData
+    })?;
+
+    msg!("Message received! {:?}", message);
+
     // Increment and store the number of times the account has been greeted
     let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    // let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow()).unwrap();
     greeting_account.counter += 1;
+    msg!("greeting_account.counter : {:?}", greeting_account.counter);
+    greeting_account.text = message.text;
+
+
+    // TODO: Fix error(BorshIoError) here
     greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    // GreetingAccount::pack (&mut account.data.borrow_mut())?;
+    // greeting_account.try_to_vec().unwrap();
 
     msg!("Greeted {} time(s)!", greeting_account.counter);
+    msg!("Greeted text : {}", greeting_account.text);
 
     Ok(())
 }
@@ -60,19 +91,39 @@ mod test {
         let program_id = Pubkey::default();
         let key = Pubkey::default();
         let mut lamports = 0;
-        let mut data = vec![0; mem::size_of::<u32>()];
+        // let data: GreetingAccount = GreetingAccount::new(0, "nope".to_string());
+        let data: GreetingAccount = GreetingAccount {
+            counter: 0, 
+            text: "nope".to_string()
+        };
+        let mut serialized_data = &mut data.try_to_vec().unwrap();
+        // let mut serialized_data = vec![0; mem::size_of::<u32>()];
+
+        // let deserialized_data = GreetingAccount::try_from_slice(&serialized_data).unwrap();
+        // println!("deserialized_data : {:?}", deserialized_data);
+
         let owner = Pubkey::default();
         let account = AccountInfo::new(
             &key,
             false,
             true,
             &mut lamports,
-            &mut data,
+            &mut serialized_data,
             &owner,
             false,
             Epoch::default(),
         );
-        let instruction_data: Vec<u8> = Vec::new();
+
+
+        let message =  Message {
+            text: "안뇽하세요".to_string(),
+            from: "JKeum".to_string()
+        };
+
+        let serialized_message = message.try_to_vec().unwrap();
+        let instruction_data = serialized_message;
+
+        println!("instruction_data, {:?}", instruction_data);
 
         let accounts = vec![account];
 
@@ -82,6 +133,7 @@ mod test {
                 .counter,
             0
         );
+
         process_instruction(&program_id, &accounts, &instruction_data).unwrap();
         assert_eq!(
             GreetingAccount::try_from_slice(&accounts[0].data.borrow())
@@ -89,12 +141,20 @@ mod test {
                 .counter,
             1
         );
+
         process_instruction(&program_id, &accounts, &instruction_data).unwrap();
         assert_eq!(
             GreetingAccount::try_from_slice(&accounts[0].data.borrow())
                 .unwrap()
                 .counter,
             2
+        );
+        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        assert_ne!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            4
         );
     }
 }
